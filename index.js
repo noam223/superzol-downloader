635d59a1aad8b3ddc4beda5ca8f29dfef3d0ee24
noam223 <noamnisim@gmail.com>
2025-07-04 16:45:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ const loadPriceFullToPostgres = async () => {
     };
 
     await client.query(`
-      INSERT INTO price_full2 (
+      INSERT INTO price_full (
         product_id, store_id, chain_id, item_name, manufacturer_name,
         manufacturer_item_id, unit_qty, quantity, unit_of_measure,
         b_is_weighted, item_price, unit_price, update_date
