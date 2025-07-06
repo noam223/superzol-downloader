5a31bf885c3a0909d6ba5336c49802d4630de50a
noam223 <noamnisim@gmail.com>
2025-07-06 18:01:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ const loadAllPriceFullFiles = async () => {
             ) VALUES (
               $1, $2, $3, $4, $5,
               $6, $7, $8, $9,
-              $10, $11, $12, $13, $14
+              $10, $11, $12, $13
             )
             `,
             Object.values(values)
